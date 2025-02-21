5eee04a9244093220a676bd30ced480112d90f99
sergi0281 <sergi.franco@iticbcn.cat>
2025-02-21 16:06:57 +0100

    Create iris2.py

---
+++ iris2.py
@@ -0,0 +1,31 @@
+!pip install scikit-learn
+
+from sklearn.datasets import load_iris
+import pandas as pd
+
+iris = load_iris()
+
+# Convertir-lo a un DataFrame de Pandas
+df = pd.DataFrame(iris.data, columns=iris.feature_names)
+
+# Afegir la columna de la classe (tipus de flor)
+df['species'] = iris.target
+
+df.head()
+
+df.info()
+
+df.describe()
+
+sns.scatterplot(data=df, x="petal length (cm)", y="sepal length (cm)")
+
+colors_personalitzats = {
+    0: "red",
+    1: "green",
+    2: "blue"
+}
+sns.scatterplot(data=df, x="petal length (cm)", y="sepal length (cm)",hue="species", palette=colors_personalitzats)
+
+sns.pairplot(df, hue="species")
+
+sns.barplot(x="species", y="petal length (cm)", data=df, estimator="mean", hue="species", palette="coolwarm")
